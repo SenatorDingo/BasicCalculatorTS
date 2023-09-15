8d52233d5c39f6b26e5543394cbb2f85a28b534a
SenatorDingo <abhayabhilash@duck.com>
2023-09-15 19:03:08 -0400

    Upload Full app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+secondNumber: number | undefined;
+firstNumber: number | undefined;
+resultValue: number | undefined;
+
+calculate(operator: string, first: string, second: string) {
+  this.firstNumber = Number(first);
+  this.secondNumber = Number(second);
+  switch (operator){
+    case "+":
+      this.resultValue = this.firstNumber + this.secondNumber;
+      break;
+    case "-":
+      this.resultValue = this.firstNumber - this.secondNumber;
+      break;
+    case "*":
+      this.resultValue = this.firstNumber * this.secondNumber;
+      break;
+    case "/":
+      this.resultValue = this.firstNumber / this.secondNumber;
+      break;
+    case "C":
+      this.resultValue = undefined;
+      this.firstNumber = undefined;
+      this.secondNumber = undefined;
+  }
+    
+
+}
   title = 'BasicCalculator';
 }
